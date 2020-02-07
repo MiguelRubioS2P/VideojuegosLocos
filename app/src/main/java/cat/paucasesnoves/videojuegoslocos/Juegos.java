4c527f5070ffb95f59c7f52bbf63aaf95fd129d3
Jose <josealbertoriera@iespaucasesnoves.cat>
2020-02-07 18:38:16 +0100

    Cosas

--- app/src/main/java/cat/paucasesnoves/videojuegoslocos/Juegos.java
+++ app/src/main/java/cat/paucasesnoves/videojuegoslocos/Juegos.java
@@ -10,8 +10,11 @@ import android.os.Debug;
 import android.view.Gravity;
 import android.widget.ImageView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -20,6 +23,9 @@ import cat.paucasesnoves.videojuegoslocos.entitats.DBInterface;
 public class Juegos extends AppCompatActivity {
     private static int plat;
     DBInterface bd;
+    //Declaro el text view
+    TextView plataformaActual;
+
     //Cuando se crea a vista
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,31 +34,106 @@ public class Juegos extends AppCompatActivity {
         setContentView(R.layout.activity_juegos);
         //Debug.(getIntent());
         //
-        plat = obtenerPlataformaActual();
-       /* Toast toast1;
-        toast1 = Toast.makeText(getApplicationContext(),"Id seleccionado."+plat, Toast.LENGTH_SHORT);
-        toast1.setGravity(Gravity.CENTER|Gravity.LEFT,250,50);
-        toast1.show();*/
-        //obtenerJuegosPlataforma(plat);
+        plataformaActual = (TextView) findViewById(R.id.LogoTexto);
+
+        Bundle extras = getIntent().getExtras();
+        plat = extras.getInt("Id");
+
+        //Obtener la plataforma actual con la BBDD
+                // obtenerPlataformaActual();
+        //Obtener la plataforma actual con Switch y obteniendo el ID por el Budle /Intent.
+        saberPlataforma();
+
+
+        obtenerJuegosPlataforma();
 
     }
 
+private void saberPlataforma(){
+    switch (plat) {
+        case 4:
+            plataformaActual.setText("Nintendo");
+            break;
+        case 1:
+            plataformaActual.setText("Playstation");
+            break;
+        case 2:
+            plataformaActual.setText("Xbox");
+            break;
+        case 3:
+            plataformaActual.setText("Pc");
+            break;
+    }
+}
+    //Obtener la plataforma actual con la BBDD
+    private void  obtenerPlataformaActual(){
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            //Se ha recibido el id de plataforma
+            plat = extras.getInt("Id");
 
+            bd = new DBInterface(getApplicationContext());
+            bd.abrir();
+            Cursor c = bd.obtenerPlataforma(plat);//Obtener una plataforma en especifico.
+            //Ya en el metodo hace             mCursor.moveToFirst();
+            //c.moveToNext();
+            plataformaActual.setText(c.getString(1));
+            bd.cerrar();
+
+        }else{
+            Toast toast1;
+            toast1 = Toast.makeText(getApplicationContext(),"No se ha pasado correctamente.", Toast.LENGTH_SHORT);
+            toast1.setGravity(Gravity.CENTER|Gravity.LEFT,250,50);
+            toast1.show();
+
+            plat = 0;
+        }
+    }
     //Obtener todos los juegos de una plataforma en especifico...
-    public void obtenerJuegosPlataforma(int plat){
+    public void obtenerJuegosPlataforma(){
+        //CLAVE_JUEGOID4
         bd = new DBInterface(getApplicationContext());
         bd.abrir();
+        //Solicitamos todos los Juegos referente a una plataforma
         Cursor c = bd.obtenerJuegosPlataformas(plat);
+        //Obtenemos el primer valor
         c.moveToFirst();
+
+        //Contenedor del listado de juegos
         ArrayList<HashMap<String,String>> llista = new ArrayList<HashMap<String, String>>();
+
+        //HashMap que contendrá la info de los juegos
+        HashMap<String,String> map = new HashMap<>();
+
+        //ArrayList para obtener solo los id de los juegos
+        ArrayList<Integer> idJuego = new ArrayList<>();
+
+        //Recorrer los resultados
         while(!c.isAfterLast()){
-            HashMap<String,String> map = new HashMap<String, String>();
-            map.put("id",c.getString(0));
-            map.put("nom",c.getString(1));
-            map.put("email",c.getString(2));
+            //Añadir nuemero al ArrayList
+            idJuego.add(c.getInt(1));
+            c.moveToNext();
+        }
+
+        for (int p : idJuego) {
+
+
+        }
+
+
+
+
+        /*
+         ArrayList<HashMap<String,String>> llista = new ArrayList<HashMap<String, String>>();
+
+        while(!c.isAfterLast()){
+            HashMap<String,String> map = new HashMap<>();
+
+            map.put("juegoid",c.getString(0));
             llista.add(map);
             c.moveToNext();
         }
+        * */
         bd.cerrar();
         /*
         adapter = new SimpleAdapter(getApplicationContext(), llista, R.layout.activity_item_juego, new String[]{"id","nom","email"},new int[]{R.id.id_item_lista,R.id.nombre_item_lista,R.id.email_item_lista});
@@ -62,20 +143,7 @@ public class Juegos extends AppCompatActivity {
         listView.addFooterView(menu);*/
     }
 
-    //Obtener la plataforma actual
-    private int obtenerPlataformaActual(){
-        Bundle extras = getIntent().getExtras();
-        if(extras != null){
-             plat = extras.getInt("Id");
-        }else{
-            Toast toast1;
-            toast1 = Toast.makeText(getApplicationContext(),"No se ha pasado correctamente.", Toast.LENGTH_SHORT);
-            toast1.setGravity(Gravity.CENTER|Gravity.LEFT,250,50);
-            toast1.show();
 
-            plat = 0;
-        }
-        return plat;
-    }
+
 
 }
